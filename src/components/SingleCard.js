2e9bfb927923c8e6752781f6b7b58cb5302f1b3c
h131177 <54102543+h131177@users.noreply.github.com>
2022-12-24 01:23:44 +0100

    My Card Component solution

---
+++ src/components/SingleCard.js
@@ -0,0 +1,13 @@
+const SingleCard = ({card}) => {
+
+    return (
+        <div className="card" key={card.id}>
+            <div>
+              <img className="front" src={card.src} alt="card front" />
+              <img className="back" src="/img/cover.png" alt="card back" />
+            </div>
+        </div>
+    );
+}
+ 
+export default SingleCard;
