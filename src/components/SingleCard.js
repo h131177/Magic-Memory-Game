da1939191dc17ab5cdf544758d44333e49b99712
h131177 <54102543+h131177@users.noreply.github.com>
2022-12-25 15:09:26 +0100

    Making card choices

--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -1,9 +1,21 @@
-export default function SingleCard({ card }) {
+import './SingleCard.css'
+
+export default function SingleCard({ card, handleChoice }) {
+
+    const handleclick = () => {
+        handleChoice(card);
+    }
+
     return (
         <div className="card" key={card.id}>
             <div>
               <img className="front" src={card.src} alt="card front" />
-              <img className="back" src="/img/cover.png" alt="card back" />
+              <img 
+              className="back" 
+              src="/img/cover.png" 
+              onClick={handleclick} 
+              alt="card back" 
+              />
             </div>
         </div>
     )
